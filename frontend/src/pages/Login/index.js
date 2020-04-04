c0a7fc89773317033953afe57268f39d3af86e56
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-04 19:44:41 -0300

    Código com lógica de listagem de casos por Ong

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -9,15 +11,37 @@ import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Login(){
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function login(e){
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId',id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        }catch(err){
+            alert('Falha no login, tente novamente.');
+        }
+    }
+
     return(
         <div className="login-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
 
-                <form>
+                <form onSubmit={login}>
                     <h1>Faça seu login</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Acessar</button>
 
                     <Link className="back-link" to="/register">
