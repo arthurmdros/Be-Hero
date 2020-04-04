c7120c8403bd71f6a404841ce2febf5589966723
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-04 18:25:53 -0300

    Adicionando páginação de profile e register

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -18,10 +20,10 @@ export default function Login(){
                     <input placeholder="Sua ID"/>
                     <button className="button" type="submit">Acessar</button>
 
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
